d447a37966d628b14c10267bee6e1938e559e675
accrane <austincrane@gmail.com>
2021-03-08 10:43:36 -0500

    change securtiy policy

--- header.php
+++ header.php
@@ -12,60 +12,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta http-equiv="Content-Security-Policy" content="default-src 'none' ; 
-
-
-    font-src 		https: 'self' 'unsafe-inline'
-    				https://fonts.googleapis.com 
-                    https://fonts.gstatic.com
-                    https://use.typekit.net
-                    https://p.typekit.net
-                    https://use.fontawesome.com
-                    https://www.google.com 
-                    data:;
-
-    img-src 		https: 'self' 'unsafe-inline'
-    				https://secure.gravatar.com
-                    http://0.gravatar.com
-                    https://use.fontawesome.com
-                    https://www.google.com
-                    https://www.facebook.com  
-                    data:;
-
-    style-src 		https: 'unsafe-inline' 
-                    https://fonts.googleapis.com 
-                    https://use.typekit.net	;
-
-    script-src      'self'
-                    https://www.google.com
-                    https://www.google-analytics.com
-                    https://ajax.googleapis.com
-                    https://use.fontawesome.com  ;
-
-    manifest-src    'self' ;
-
-    script-src-elem	https: 'self' 'unsafe-inline'
-    				https://connect.facebook.net
-    				http://www.google-analytics.com
-    				https://ajax.googleapis.com
-                    https://use.fontawesome.com ;
-
-    style-src-elem https: 'self' 'unsafe-inline'
-    				https://p.typekit.net
-    				https://use.fontawesome.com
-    				https://fonts.googleapis.com
-    				https://use.typekit.net
-    				https://fonts.googleapis.com ;
-
-    frame-src		https://www.facebook.com
-    				https://www.google.com ;
-
-    connect-src		https://www.google-analytics.com 
-				    www.google-analytics.com 
-				    https://stats.g.doubleclick.net ;
-    child-src 'none' ; 
-    object-src 'none' ;
-    frame-ancestors 'none' ; ">
+<?php //get_template_part('inc/security-policy'); ?>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
